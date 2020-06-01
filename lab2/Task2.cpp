95f3064ae3e6f55d42f8933e1a2284cb8e5cbcfb
N0llPointer <37152093+N0llPointer@users.noreply.github.com>
2020-06-01 18:21:41 +0300

    Create Task2.cpp

---
+++ lab2/Task2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+
+	int k;
+	double x;
+
+	cout << "k = ";
+	cin >> k;
+	cout << "x = ";
+	cin >> x;
+	cout << endl;
+
+	double summary = 0;
+
+	for (int i = 1; i <= k; i++) {
+		summary += pow(x, 2*(i-1))/pow(2 + 4*(i-1),2);
+	}
+
+	cout << "Сумма = " << summary;
+
+	_getch();
+
+	return 0;
+}
